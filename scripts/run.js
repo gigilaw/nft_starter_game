94e8b5603e35534144240320b70336fbe5f0ab3a
Gigi Law <gigi.ht.law.pccw@gmail.com>
2022-02-18 12:08:08 +0800

    initial character creation

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,15 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame')
-  const gameContract = await gameContractFactory.deploy()
+  const gameContract = await gameContractFactory.deploy(
+    ['Beefalo', 'WerePig', 'Hound'], // Names
+    [
+      'https://i.imgur.com/Wb3KQD8.jpeg', // Images
+      'https://i.imgur.com/WWIwh8O.png',
+      'https://i.imgur.com/UvrgXl9.png',
+    ],
+    [500, 200, 150], // HP values
+    [100, 50, 25], // Attack damage values
+  )
   await gameContract.deployed()
   console.log('Contract deployed to:', gameContract.address)
 }
